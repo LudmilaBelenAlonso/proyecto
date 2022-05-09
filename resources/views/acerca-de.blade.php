60523e334f3735b97b7d8cd4f82afbb2a191b709
ludmilabelenalonso <ludmila.b.alonso@gmail.com>
2022-05-09 00:44:30 -0300

    irisnews
    acerca-de
    home

--- resources/views/acerca-de.blade.php
+++ resources/views/acerca-de.blade.php
@@ -4,7 +4,9 @@
 @section('main')
 
     <h1>Acerca de Iris</h1>
-    <div class="contenedor rounded">
-        
+    <div class="contenedor rounded p-3">
+        <p>Somos un equipo que queremos acercar y facilitar información de festivales y eventos tanto aquí como en el exterior.</p>
+        <p>Nos gusta asistir a los eventos que publicamos, los últimos dos años nos tuvimos que guardar para cuidarnos del covid-19, pero ahora todo se esta reactivando. Las bandas de música están comenzando sus tours, los eventos que nos gustan están empezando a ser presenciales o híbridos. Es hora de aprovechar el momento.</p>
+        <p>Esperemos que los eventos y festivales que vamos a estar informando sean de tu agrado.</p>
     </div>
 @endsection
