a57b42a99728c49da4d468bb8ddf2bbf946bb740
ludmilabelenalonso <ludmila.b.alonso@gmail.com>
2022-05-08 23:44:31 -0300

    iris news
    css

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -22,15 +22,15 @@
                 <h2 class="card-title">{{$noticia->noticia_id}} - {{$noticia->titulo}}</h2>
                 <div>
                     @forelse($noticia->generos as $genero)
-                        <span class="badge bg-secondary">{{$genero->nombre}}</span>
+                        <span class="badge bg-info">{{$genero->nombre}}</span>
                     @empty
                         <p class="badge bg-secondary">No especificado</p>
                     @endforelse
                 </div>
                 <p class="card-text">{{$noticia->copete}}</p>
-                <p class="card-text">{{$noticia -> fecha_evento}}</p>
-                <p class="card-text">{{$noticia -> localidad->nombre}}</p>
-                <p class="card-text">{{$noticia -> localidad->pais}}</p>
+                <p class="card-text"><strong>Mes y año del evento: </strong>{{$noticia -> fecha_evento}}</p>
+                <p class="card-text"><strong>Localidad: </strong>{{$noticia -> localidad->nombre}}</p>
+                <p class="card-text"><strong>País: </strong>{{$noticia -> localidad->pais}}</p>
                     <div class="d-flex">
                         <a href="{{ route('noticias.ver', ['id' => $noticia->noticia_id]) }}" class="btn btn-outline-primary mx-1">Ver</a>
 
